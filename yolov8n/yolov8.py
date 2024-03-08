ada9547daa3c923066a487f0e87ea46ba0af538d
duclv <leduchtth@gmail.com>
2024-03-08 17:14:31 +0700

    log

--- yolov8n/yolov8.py
+++ yolov8n/yolov8.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 if __name__ == '__main__':
     # Use the model
-    model.train(data="dataset.yaml", epochs=20, batch=12, pretrained=True, lr0=0.01)  # train the model
+    model.train(data="dataset.yaml", epochs=500, batch=12, pretrained=True, lr0=0.01, device = [0])  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
     #results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
     path = model.export(format="onnx")  # export the model to ONNX format
